45be1835d635f193b45079ed9ed27dd59cdf8f38
Dominic Cartwright <dcartwright@slingshotedu.com>
2023-04-21 11:52:49 -0400

    Disable mouse

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -10,11 +10,12 @@ vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.wildmode =
+'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
 vim.opt.completeopt = 'menuone,longest,preview'
 
 vim.opt.title = true
-vim.opt.mouse = 'a' -- enable mouse for all modes
+vim.opt.mouse = ''
 
 vim.opt.termguicolors = true
 
@@ -23,7 +24,7 @@ vim.opt.spell = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.list = true -- enable the below listchars
+vim.opt.list = true                     -- enable the below listchars
 vim.opt.listchars = { tab = '▸ ', trail = '·' }
 vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
 
@@ -35,10 +36,10 @@ vim.opt.sidescrolloff = 8
 
 vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
 
-vim.opt.confirm = true -- ask for confirmation instead of erroring
+vim.opt.confirm = true            -- ask for confirmation instead of erroring
 
 vim.opt.signcolumn = 'yes:2'
 
-vim.opt.undofile = true -- persistent undo
-vim.opt.backup = true -- automatically save a backup file
+vim.opt.undofile = true       -- persistent undo
+vim.opt.backup = true         -- automatically save a backup file
 vim.opt.backupdir:remove('.') -- keep backups out of the current directory
