b56ba842ffca83e37c3ff3ca0ed8af95e548b410
Dominic Cartwright <dcartwright@slingshotedu.com>
2023-05-01 09:56:53 -0400

    Update style of bufferline

--- nvim/lua/user/plugins/bufferline.lua
+++ nvim/lua/user/plugins/bufferline.lua
@@ -8,7 +8,7 @@ require('bufferline').setup({
     max_name_length = 25,
     offsets = {
       {
-        filetype = 'NvimTree',
+        filetype = 'NeoTree',
         text = '  Files',
         highlight = 'StatusLine',
         text_align = 'left',
@@ -22,58 +22,6 @@ require('bufferline').setup({
           { text = '    ', fg = '#8fff6d' },
         }
       end,
-    },
-  },
-  highlights = {
-    fill = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    background = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    tab = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    tab_close = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    close_button = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-      fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
-    },
-    close_button_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-      fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
-    },
-    close_button_selected = {
-      fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
-    },
-    buffer_visible = {
-      bg = { attribute = 'bg', highlight = 'Normal' },
-    },
-    modified = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    modified_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    duplicate = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    duplicate_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    separator = {
-      fg = { attribute = 'bg', highlight = 'StatusLine' },
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    separator_selected = {
-      fg = { attribute = 'bg', highlight = 'StatusLine' },
-      bg = { attribute = 'bg', highlight = 'Normal' }
-    },
-    separator_visible = {
-      fg = { attribute = 'bg', highlight = 'StatusLine' },
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
     },
   },
 })
