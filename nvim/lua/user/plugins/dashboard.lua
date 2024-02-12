5c4f6ef188b3b61897886dce3b48b999e0824aa0
Dominic Cartwright <dcartwright@slingshotedu.com>
2024-02-11 21:33:52 -0500

    Update dashboard

--- nvim/lua/user/plugins/dashboard.lua
+++ nvim/lua/user/plugins/dashboard.lua
@@ -1,42 +1,23 @@
 return {
-  -- 'glepnir/dashboard-nvim',
-  -- event = 'VimEnter',
-  -- dependencies = 'devicons',
-  --
-  -- config = function()
-  --   require('dashboard').setup({
-  --     config = {
-  --       header = {
-  --         '',
-  --         '',
-  --         "888888b.    .d888b.   888b   d888  888888  888b    88  888888   .d888b.",
-  --         " 88     8b  d8     8b  88 Yb dY 88    88    88 Yb   88    88    d8     8b",
-  --         " 88     88  88     88  88  'Y'  88    88    88  Yb  88    88    88       ",
-  --         " 88     8P  Y8     8P  88       88    88    88   Yb 88    88    Y8     8P",
-  --         "888888P'    'Y888P'   88       88  888888  88    'Y88  888888   'Y888P'",
-  --         "",
-  --       },
-  --       packages = { enable = true },
-  --       project = { enable = false, limit = 8, icon = ' Projects', label = '', action = 'Telescope find_files cwd=' },
-  --       mru = { limit = 8, icon = ' Recent Files', label = '', },
-  --       footer = {
-  --         '',
-  --         'All I really know is 💯! Nothing less!',
-  --         '',
-  --       },
-  --     },
-  --   })
-  --
-  --   vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#EA344F' })
-  --   vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = '#f8f8f2' })
-  --   vim.api.nvim_set_hl(0, 'DashboardShortcut', { fg = '#bd93f9' })
-  --   vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#f2f2f2' })
-  -- end,
-
-
   'goolord/alpha-nvim',
   dependencies = 'devicons',
   config = function()
-    require 'alpha'.setup(require 'alpha.themes.startify'.config)
+    local alpha = require 'alpha'
+    local startify = require 'alpha.themes.startify'
+
+    startify.section.header.val = {
+      [[                                                                          ]],
+      [[ 888888b.    .d888b.   888b   d888  888888  888b    88  888888   .d888b.  ]],
+      [[ 88     8b  d8     8b  88 Yb dY 88    88    88 Yb   88    88    d8     8b ]],
+      [[ 88     88  88     88  88  'Y'  88    88    88  Yb  88    88    88        ]],
+      [[ 88     8P  Y8     8P  88       88    88    88   Yb 88    88    Y8     8P ]],
+      [[ 888888P'    'Y888P'   88       88  888888  88    'Y88  888888   'Y888P'  ]],
+    }
+    startify.section.footer.val = {
+      { type = "padding", val = 1 },
+      { type = "text", val = "All I really know is 💯! Nothing less!" },
+    }
+
+    alpha.setup(startify.config)
   end
 }
