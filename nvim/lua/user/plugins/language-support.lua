84243e1d505c1b3475f9cf69e4b710e71bb91e4e
Dominic Cartwright <dcartwright@slingshotedu.com>
2024-02-11 22:36:11 -0500

    Update treesitter config

--- nvim/lua/user/plugins/language-support.lua
+++ nvim/lua/user/plugins/language-support.lua
@@ -16,6 +16,9 @@ return {
     config = function()
       require('nvim-treesitter.configs').setup({
         ensure_installed = 'all',
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {},
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = true,
