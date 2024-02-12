1ad2d58635f67f8b74daa73c0928e81fab115ec3
Dominic Cartwright <dcartwright@slingshotedu.com>
2024-02-11 22:17:08 -0500

    Clean up configuration

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -16,14 +16,13 @@ vim.g.mapleader = " "
 require("lazy").setup({
     'folke/neodev.nvim', -- Development plugin
 
-    { import = 'user.plugins.which-key' },
-    { import = 'user.plugins.ui' },
-    { import = 'user.plugins.comment' },
-
     -- Reused Dependencies
     { 'nvim-lua/plenary.nvim',                 name = 'plenary' },
     { 'nvim-tree/nvim-web-devicons',           name = 'devicons' },
 
+    { import = 'user.plugins.which-key' },
+    { import = 'user.plugins.ui' },
+    { import = 'user.plugins.comment' },
     { import = 'user.plugins.dashboard' },
     { import = 'user.plugins.language-support' },
     { import = 'user.plugins.telescope' },
@@ -34,7 +33,6 @@ require("lazy").setup({
     { import = 'user.plugins.colorizer' },
     { import = 'user.plugins.lualine' },
     { import = 'user.plugins.flash' },
-    { import = 'user.plugins.test' },
 
     'tpope/vim-surround',              -- Add, change, and delete surrounding text.
     'tpope/vim-unimpaired',            -- Pairs of handy bracket mappings, like [b and ]b.
@@ -48,9 +46,20 @@ require("lazy").setup({
     'alvan/vim-closetag',              -- Automatically close HTML tags.
     'famiu/bufdelete.nvim',            -- All closing buffers without closing the split window.
     'github/copilot.vim',              -- Github Copilot
+    'f-person/git-blame.nvim',         -- Git Blame
 
     -- Text objects for HTML attributes
     { 'whatyouhide/vim-textobj-xmlattr', dependencies = 'kana/vim-textobj-user' },
+
+    -- Automatically add closing brackets, quotes, etc.
+    { 'windwp/nvim-autopairs',           config = true },
+
+    -- Add smooth scrolling to avoid jarring jumps
+    { 'karb94/neoscroll.nvim',           config = true },
+
+    -- Git integration.
+    { 'lewis6991/gitsigns.nvim',         config = true },
+    { "kdheepak/lazygit.nvim",           dependencies = 'plenary' },
 
     -- File security
     { 'laytan/cloak.nvim',               config = true },
@@ -67,23 +76,12 @@ require("lazy").setup({
         end,
     },
 
-    -- Automatically add closing brackets, quotes, etc.
-    { 'windwp/nvim-autopairs',           config = true },
-
-    -- Add smooth scrolling to avoid jarring jumps
-    { 'karb94/neoscroll.nvim',           config = true },
-
     -- File tree sidebar
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
         dependencies = { 'plenary', 'devicons', "MunifTanjim/nui.nvim" },
     },
-
-    -- Git integration.
-    'f-person/git-blame.nvim',
-    { 'lewis6991/gitsigns.nvim', config = true },
-    { "kdheepak/lazygit.nvim",   dependencies = 'plenary' },
 
 }, {
     checker = {
